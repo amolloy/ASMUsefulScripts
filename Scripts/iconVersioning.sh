b566796f872c5f2a3d584632ec0a52def59f648d
Andrew Molloy <amolloy@gmail.com>
2014-10-26 10:01:07 -0400

    Remove some console spam. Also, get rid of the unused abspath function

--- Scripts/iconVersioning.sh
+++ Scripts/iconVersioning.sh
@@ -2,7 +2,6 @@
 export PATH=/opt/local/bin/:/opt/local/sbin:$PATH:/usr/local/bin:
 
 convertPath=`which convert`
-echo ${convertPath}
 if [[ ! -f ${convertPath} || -z ${convertPath} ]]; then
   echo "WARNING: Skipping Icon versioning, you need to install ImageMagick and ghostscript (fonts) first, you can use brew to simplify process:
   brew install imagemagick ghostscript"
@@ -17,9 +16,6 @@ commit=`"$git" rev-parse --short HEAD`
 branch="${branch}->${BUNDLE_DISPLAY_NAME_SUFFIX}"
 
 caption="$build_num\n${branch}\n${commit}"
-echo $caption
-
-function abspath() { pushd . > /dev/null; if [ -d "$1" ]; then cd "$1"; dirs -l +0; else cd "`dirname \"$1\"`"; cur_dir=`dirs -l +0`; if [ "$cur_dir" == "/" ]; then echo "$cur_dir`basename \"$1\"`"; else echo "$cur_dir/`basename \"$1\"`"; fi; fi; popd > /dev/null; }
 
 function processIcon() {
 	iconFile=$1
@@ -27,25 +23,16 @@ function processIcon() {
 	normalizedIconFile=$iconFile.normalized
 
     if [[ -f ${storedIconFile} ]]; then
-      echo "found previous file at path ${storedIconFile}, using it as base"
       mv "${storedIconFile}" "${iconFile}"
     fi
     
-#    if [ $CONFIGURATION = "Release" ]; then
-#      cp "${base_path}" "$target_path"
-#      return 0;
-#    fi
-    
-    echo "Reverting optimized PNG to normal"
     # Normalize
     xcrun -sdk iphoneos pngcrush -revert-iphone-optimizations -q "${iconFile}" "${normalizedIconFile}"
     
     # move original pngcrush png to tmp file
-    echo "moving pngcrushed png file at ${iconFile} to ${storedIconFile}"
     mv "${iconFile}" "${storedIconFile}"
     
     # Rename normalized png's filename to original one
-    echo "Moving normalized png file to original one ${normalizedIconFile} to ${iconFile}"
     mv "${normalizedIconFile}" "${iconFile}"
     
     width=`identify -format %w ${iconFile}`
@@ -54,8 +41,6 @@ function processIcon() {
     band_position=$(($height - $band_height))
     text_position=$(($band_position - 3))
     point_size=$(((13 * $width) / 100))
-    
-    echo "Image dimensions ($width x $height) - band height $band_height @ $band_position - point size $point_size"
     
     #
     # blur band and text
@@ -76,8 +61,6 @@ function processIcon() {
     
     # clean up
     rm -r ${tmpDir}
-    
-    echo "Overlayed ${iconFile}"
 }
 
 iconFiles="${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/AppIcon*.png"
