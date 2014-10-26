c22121cb443bd67dfb3d1a878a596c0f27e236c0
Andrew Molloy <amolloy@gmail.com>
2014-10-26 10:03:25 -0400

    Add updateVersionInfo.sh

---
+++ Scripts/updateVersionInfo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+appBuild=`git rev-list --all |wc -l`
+
+revision_no="$(git symbolic-ref HEAD 2> /dev/null | cut -b 12-)-$(git log --pretty=format:'%h, %ad' -1)"
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $appBuild" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+/usr/libexec/PlistBuddy -c "Set RevisionNumber $revision_no" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
